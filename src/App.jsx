2664b29fdd3b741dcde9826c7eaea088c92908cb
anshul-rohilla4 <anshulrohilla111@gmail.com>
2024-09-29 23:48:12 +0530

    1:29:00 completed List

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,22 @@ import List from './List.jsx'
 
 function App() {
 
+  const fruits=[
+    {id:1,name:"apple" , calories:95,},
+    {id:2,name:"dragon fruit" , calories:45,},
+    {id:3,name:"coal" , calories:105,},
+    {id:4,name:"banana" , calories:156,},
+    {id:5,name:"mango" , calories:100}
+  ];
+
+  const vegetables=[
+    {id:6,name:"tomato" , calories:105,},
+    {id:7,name:"calery" , calories:95,},
+    {id:8,name:"carrot" , calories:105,},
+    {id:9,name:"corn" , calories:95,},
+    {id:10,name:"coli" , calories:156,}
+  ]
+
   return(
     <>
     <Card/>
@@ -21,7 +37,11 @@ function App() {
 
     <UserGreeting isLoggedIn={false} username="anshul"/>
 
-    <List/>
+    {fruits.length>0 ? <List items={fruits} category="Fruits"/>:null}
+    {/* returns only if list is not empty ie. length >0 */}
+
+    {vegetables.length>0 &&<List items={vegetables} category="Vegetables"/>}
+    {/* as the second part of the statement will always be true so the return depends on length property only */}
     </>
       
 
--- src/List.jsx
+++ src/List.jsx
@@ -1,35 +1,54 @@
-function List(){
-    const fruits=[
-                {id:1,name:"apple" , calories:95,},
-                {id:2,name:"dragon fruit" , calories:45,},
-                {id:3,name:"coal" , calories:105,},
-                {id:4,name:"banana" , calories:156,}
-            ];
+import PropTypes from 'prop-types';
+function List(props){
+    
 
     // fruits.sort((a,b)=> a.name.localeCompare(b.name));//sort alphabetically
-    fruits.sort((a,b)=> b.name.localeCompare(a.name));//sort reverse alphabetically
-    fruits.sort((a,b)=> a.calories-b.calories);//sort numerical calories
+    // fruits.sort((a,b)=> b.name.localeCompare(a.name));//sort reverse alphabetically
+    // fruits.sort((a,b)=> a.calories-b.calories);//sort numerical calories
 
 
-    const lowCalFruits=fruits.filter(fruit=>fruit.calories<100);//filter out those having calories>100
+    // const lowCalFruits=fruits.filter(fruit=>fruit.calories<100);//filter out those having calories>100
 
-    const highCalFruits=fruits.filter(fruit=>fruit.calories>=100);//filter out those having calories<100
+    // const highCalFruits=fruits.filter(fruit=>fruit.calories>=100);//filter out those having calories<100
 
 
-    // const listItems=fruits.map(fruit => <li key={fruit.id}>
-    //                                             {fruit.name}:&nbsp;
-    //                                             <b>{fruit.calories}</b>
-                                                
-    //                                             </li>);
+    
+    const itemList=props.items;
+    const category=props.category;
+
+    const listItems=itemList.map(items => <li key={items.id}>
+                                            {items.name}:&nbsp;
+                                            <b>{items.calories}</b>
+                                        </li>);
+    
+
+    // const listItems=itemList.map(itemslowCalFruit => <li key={items.id}>
+    //                                                 {items.name}:&nbsp;
+    //                                                 <b>{items.calories} </b>
+    //                                                 </li>);
     
     
-    const listItems=lowCalFruits.map(lowCalFruit => <li key={lowCalFruit.id}>
-                                                    {lowCalFruit.name}:&nbsp;
-                                                    <b>{lowCalFruit.calories} </b>
-                                                    </li>);
-    
-    
-    return(<ol>{listItems}</ol>);
+
+
+    return(<>
+            <h3 className="listCategory">{category}</h3>
+            <ol className="listItems">{listItems}</ol>
+        </>);           
 }
+
+List.PropTypes={
+    category:PropTypes.string,
+    items:PropTypes.arrayOf(PropTypes.shape({id:PropTypes.number,
+                                            category:PropTypes.string,
+                                            calories:PropTypes.number,
+                                            })),
+}
+
+List.defaultProps={
+    category:"Category",
+    items:[],
+}
+
+
 
 export default List
