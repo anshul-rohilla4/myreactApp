16fdd8b045b84ec3388485715607f4bfb40d1594
anshul-rohilla4 <anshulrohilla111@gmail.com>
2024-10-03 18:37:18 +0530

    learning onChange

---
+++ src/MyComponent2.jsx
@@ -0,0 +1,16 @@
+import React ,{useState} from 'react;'
+
+
+function MyComponent2(){
+
+const [NamedNodeMap,setName]=useState("");
+
+
+    return(
+        <>
+
+        </>
+    );
+}
+
+export default MyComponent2;
